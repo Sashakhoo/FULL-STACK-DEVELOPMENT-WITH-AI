4006092c9fe98f30376977c77da92ddfcd6f8f9d
natasya-khoo <natasya.khoo@cantalelectric.com>
2025-05-09 17:08:02 +0800

    first commit

---
+++ Module 1_Week 1_Required Assignment_NatasyaKhoo.js
@@ -0,0 +1,22 @@
+
+//Task 1 : Variables and Data Types
+let x = 10;
+let y = "Hello";
+let result = y + " " + x;
+console.log(result);
+
+// Task 2 : Control Flow
+function checkvenOdd(num){
+    if(num %2 == 0){
+        console.log("Even");       
+    }
+    else{
+        console.log("Odd");
+    }
+}
+
+// Task 3: Functions
+function greetUser(name){
+    return "Hello, " + name + "!";
+}
+
